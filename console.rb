0b2bc9d11f79cb4093a09c2dbd9c3179da228fb7
Matteo Fusillo <matt.fusillo@gmail.com>
2019-12-06 12:36:30 +0000

    update customer class and add constructor and create method

---
+++ console.rb
@@ -0,0 +1,10 @@
+require('pry')
+require_relative('models/customer')
+require_relative('models/film')
+require_relative('models/ticket')
+
+customer1 = Customer.new({"name" => "Matteo", "funds" => 100})
+customer1.save()
+
+binding.pry
+nil
---
+++ models/customer.rb
@@ -0,0 +1,28 @@
+require_relative('../db/sql_runner')
+require_relative('customer')
+require_relative('ticket')
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (
+    name,
+    funds
+    ) VALUES (
+    $1,
+    $2
+    ) RETURNING id;"
+    values = [@name, @funds]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+end
